0dc28a601870bb92e73581131491f2b69991930c
afdolash <afdolash18@gmail.com>
2018-10-26 18:28:32 +0700

    initial commit

---
+++ app/src/main/java/com/idn/afdolash/footballclub/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.idn.afdolash.footballclub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rcTeam : RecyclerView
+    private lateinit var spinTeam : Spinner
+    private lateinit var swipeRefresh : SwipeRefreshLayout
+    private lateinit var progressBar : ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            spinTeam = spinner()
+            swipeRefresh = swipeRefreshLayout {
+                setColorSchemeResources(
+                    R.color.colorAccent,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_red_light,
+                    android.R.color.holo_orange_light
+                )
+
+                relativeLayout {
+                    lparams(width = matchParent, height = wrapContent)
+
+                    rcTeam = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
+
+                    progressBar = progressBar{
+                    }.lparams {
+                        centerInParent()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/idn/afdolash/footballclub/TeamResponse.kt
@@ -0,0 +1,3 @@
+package com.idn.afdolash.footballclub
+
+data class TeamResponse(val teams: List<Team>)
